03d1cacfc29d6adbfb1910ed63f78788872d198d
Julia Kukulies <julia.kukulies@gu.se>
2023-10-27 18:39:39 -0600

    added and modified functions

--- microphysics_functions.py
+++ microphysics_functions.py
@@ -2,9 +2,7 @@
 A collection of functions to handle the conversion and derivation of different microphyiscal quantities. 
 
 """
-
 import numpy as np 
-
 
 ##### Physical constants #####
 
@@ -24,43 +22,105 @@ molar_mass_air = 28.97 / 1000
 
 #############################
 
- def get_air_density(pressure, temperature): 
+def get_air_density(pressure, temperature): 
     """
     Get air density based on the ideal gas law 
 
     Args: 
-      pressure: air pressure field in P
-      temperature: temperature field in K 
+      pressure(np.array or xr.DataArray): air pressure field in P
+      temperature(np.array or xr.DataArray): temperature field in K 
 
+    Returns:
+      rho_dry: air density for a dry air mass in kg/m3.
+    
     """
     rho_dry  = pressure / (R*temperature)
     return rho_dry 
 
 
-def mixing_ratio_to_density(mixing_ratio): 
+def mixing_ratio_to_density(mixing_ratio, air_density): 
     """
-    Converts the mixing ratio of a substance (e.g. cloud ice) in kg/kg to density in kg/m3. 
- 
+    Converts the mixing ratio of a substance (e.g. cloud ice) to density.
+    
+    Args:
+       mixing_ratio(np.array or xr.DataArray): field with mixing ratio of a given hydrometer in kg/kg
+       air_density(np.array or xr.DataArray): field with air density in kg/m3 at a given temperature and pressure
+
+    Returns:
+    
+       density(np.array or xr.DataArray): field with air density in kg/m3 at a given temperature and pressure
+
+    Returns:
+    
+       density: density field of the hydrometer in kg/m3 (same dimensions as input).
+    
     """
+    density= mixing_ratio * air_density
+    return density
+
+
+
 
 def pressure_integration(mixing_ratio):
     """
     Integrates the mixing ratio of a hydrometeor over pressure which results in kg/m2. 
 
+    Args:
+      mixing ratio(np.array): 3D or 4D field of mixing ratio where one dimensions are pressure levels
+
+    Returns:
+      integrated_mass(np.array): 2D or 3D (if time dimension) of integrated mixing ratio in kg/m2
+   
     """
 
+
+    
 def get_saturation_vapor_pressure(temperature): 
     """
     Estimates the saturation vapor pressure for a given temperature 
     using the August-Roche-Magnus approximation. 
 
+    Args:
+       temperature: temperature or temperature field in K
+
+    Returns:
+       es: saturation vapor pressure in Pa 
+    
+    """
+    es = 6.1094 * exp(17.625 * temperature/ (T+243.04) )
+    
+    return es
+
+
+def get_dp_dt():
+    """
+    Derives the change rate in saturation vapor pressure with temperature using the Clausius-Clapeyron equation. 
+
+    
     """
 
 
+def get_condensation_rate(vertical_velocity, temperature, pressure, rho):
+    """
+
+    Estimates the condensation rate from standard model output based on saturation adjustment. 
+    
+    """
+
+    
+
+    # get change in super-saturation with temperature                                                                        
+    #dqs_dT = 0
+    #  latent heat of vaporization                                                                                                                                                                                           
+    Lv =  2.5*10^6
+    # get saturation vapor pressure
 
 
+    
+    
+    condensation_rate = q*vertical_velocity *(dqs_dT*cp**(-1) - qs* rho/ (pressure-vapor_pressure_sat) ) * (1 + dqs_dT * Lv/cp)**(-1)
 
-
+    return condensation_rate
 
 
 
