5fa21ebf2af014a27c365bca51d8ee1bade385fc
Julia Kukulies <julia.kukulies@gu.se>
2024-03-27 10:54:50 -0600

    changes to joiner program

--- processing_scripts/join_250m_tiles.py
+++ processing_scripts/join_250m_tiles.py
@@ -14,7 +14,7 @@ import numpy as np
 import xarray as xr
 import sys
 from pathlib import Path
-
+import math
 ################################################# define paths #######################################################################
 wrfout_splitfiles = '/glade/campaign/mmm/c3we/mingge/WRF_DOE/1KM/Thomson_YSU/sgp_20130617_07:00:00_L1_cheyenne/'
 rslout_file = '/glade/campaign/mmm/c3we/mingge/WRF_DOE/1KM/Thomson_YSU/sgp_20130617_07:00:00_L1_cheyenne/rsl.out.0000'
@@ -45,7 +45,7 @@ y_tile_grids = int(math.ceil(y_size/y_comp))
 ################################################################################################
 
 # variables to write into combined output 
-var_list = ['RAINNC', 'PRW_VCD', 'PRS_SDE', 'PRG_GDE', 'PRI_IDE', 'PRI_IHA', 'PRI_INU']
+var_list = ['RAINNC', 'PRW_VCD', 'PRS_SDE', 'PRG_GDE', 'PRI_IDE', 'PRI_IHA', 'PRI_INU', 'QCLOUD', 'W', 'T', 'P', 'PB']
 method = 'nearest'
 
 # get list with all time steps 
@@ -54,44 +54,47 @@ fnames = list(path.glob('wrfout*00'))
 timesteps = [tt.name for tt in fnames]
 timesteps.sort()
 
+
 for tstep in timesteps:
     # write output file 
-    outfile = savedir / tstep
-    DATA0 = xr.open_dataset(wrfout_splitfiles / str(tstep +'_'+str(0).zfill(4) ) ) 
-    DATA0 = DATA0.interp(south_north=range(y_size-1), method= method)
-    DATA0 = DATA0.interp(south_north_stag=range(y_size), method= method)
-    DATA0 = DATA0.interp(west_east=range(x_size-1), method= method)
-    DATA0 = DATA0.interp(west_east_stag=range(y_size), method= method)
-    DATA0.to_netcdf(outfile)
-    print('wrote dummy output file', str(outfile), flush = True)
+    outfile = savedir / (tstep + '_combined')
+    
+    if outfile.is_file() is False: 
+        DATA0 = xr.open_dataset(wrfout_splitfiles / str(tstep +'_'+str(0).zfill(4) ) ) 
+        DATA0 = DATA0.interp(south_north=range(y_size-1), method= method)
+        DATA0 = DATA0.interp(south_north_stag=range(y_size), method= method)
+        DATA0 = DATA0.interp(west_east=range(x_size-1), method= method)
+        DATA0 = DATA0.interp(west_east_stag=range(y_size), method= method)
+        DATA0.to_netcdf(outfile)
+        print('wrote dummy output file', str(outfile), flush = True) 
 
-    for va in tqdm(range(len( var_list))):
-        data_act = DATA0[var_list[va]]
-        data_act[:] = np.nan
-        if len(data_act.shape) < 2:
-            continue
-        la_start = 0
-        for la in range(y_comp):
-            lo_start = 0
-            for lo in range(x_comp):
-                ii = lo + int(la * x_comp)
-                # get the datatile 
-                ncid=Dataset(wrfout_splitfiles / str(tstep + '_'+str(ii).zfill(4) ), mode='r')
-                #var_list = list(DATA0.keys() ) 
-                data_tile=ncid.variables[var_list[va]][:]
-                ncid.close()
-                la_stop = la_start + data_tile.shape[-2]
-                lo_stop = lo_start + data_tile.shape[-1]
+        for va in tqdm(range(len( var_list))):
+            data_act = DATA0[var_list[va]]
+            data_act[:] = np.nan
+            if len(data_act.shape) < 2:
+                continue
+            la_start = 0
+            for la in range(y_comp):
+                lo_start = 0
+                for lo in range(x_comp):
+                    ii = lo + int(la * x_comp)
+                    # get the datatile 
+                    ncid=Dataset(wrfout_splitfiles / str(tstep + '_'+str(ii).zfill(4) ), mode='r')
+                    #var_list = list(DATA0.keys() ) 
+                    data_tile=ncid.variables[var_list[va]][:]
+                    ncid.close()
+                    la_stop = la_start + data_tile.shape[-2]
+                    lo_stop = lo_start + data_tile.shape[-1]
 
-                if len(data_act.shape) == 3:
-                    data_act[:, la_start:la_stop, lo_start:lo_stop] = data_tile
-                if len(data_act.shape) == 4:
-                    data_act[:, :, la_start:la_stop, lo_start:lo_stop] = data_tile
-                lo_start = lo_stop
-            la_start = la_stop
+                    if len(data_act.shape) == 3:
+                        data_act[:, la_start:la_stop, lo_start:lo_stop] = data_tile
+                    if len(data_act.shape) == 4:
+                        data_act[:, :, la_start:la_stop, lo_start:lo_stop] = data_tile
+                    lo_start = lo_stop
+                la_start = la_stop
 
-        # write the data to the NetCDF
-        ncid=Dataset(outfile, mode='r+')
-        ncid.variables[var_list[va]][:] = data_act
-        ncid.close()
-    print('finished joiner program for timestep:', str(tstep), flush = True)
+            # write the data to the NetCDF
+            ncid=Dataset(outfile, mode='r+')
+            ncid.variables[var_list[va]][:] = data_act
+            ncid.close()
+        print('finished joiner program for timestep:', str(tstep), flush = True)
